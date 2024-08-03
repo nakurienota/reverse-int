c70c054c2b9ac587ca35dcf5b48d7824931dc227
stenyukovs <stenyukovs@gmail.com>
2024-08-03 10:39:16 +0300

    fix:

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 module.exports = function reverse(n) {
-    var stringa = String(n);
-    var result = "";
-    for (let i = stringa.length - 1; i >= 0; i--) result += stringa[i];
-    return result;
+    return (n + '').split('').reverse().join('');
 };
