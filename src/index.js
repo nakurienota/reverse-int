ac07eca637aa1f9b309c74e94ab406010a129f73
stenyukovs <stenyukovs@gmail.com>
2024-08-03 10:35:50 +0300

    solution:

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    var stringa = String(n);
+    var result = "";
+    for (let i = stringa.length - 1; i >= 0; i--) result += stringa[i];
+    return result;
+};
